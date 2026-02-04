c27186ec07d160d398eaa5d3f464a6c0d3fa1935
GambrinusUp <kotanffas@gmail.com>
2026-02-04 22:34:25 +0700

    test

--- frontend/src/api/socket.ts
+++ frontend/src/api/socket.ts
@@ -3,8 +3,11 @@ import { WEBSOCKET_MEDIA_URL } from '~/constants/constants';
 
 const mediaUrl = import.meta.env.VITE_MEDIA_URL || WEBSOCKET_MEDIA_URL || '';
 
-export const socket: Socket = io(mediaUrl);
+export const socket: Socket = io(mediaUrl, { path: '/mediasoup/socket.io' });
 
 /*socket.on('connection-success', ({ socketId }: { socketId: string }) => {
   console.log('Connected with socketId', socketId);
 });*/
+socket.on('connect_error', (err: Error) => {
+  console.error('Connection Error:', err.message);
+});
